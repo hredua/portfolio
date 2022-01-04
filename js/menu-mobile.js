330c2a6f44a8adf66f5cc9b6e33a4abb24d207fd
hredua <helamaredua@gmail.com>
2022-01-04 16:39:14 -0400

    Fixed mobile menu

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -3,7 +3,9 @@ const btnMobile = document.getElementById('btn-mobile')
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('nav')
+    const body = document.getElementById('body')
     nav.classList.toggle('active')
+    body.classList.toggle('active')
 }
 
 btnMobile.addEventListener('click', toggleMenu)
