56c4ada117c6bd54b7e837560768a99090587b7e
Helamã Rédua <93559787+hredua@users.noreply.github.com>
2022-01-27 19:37:38 -0400

    update btn-menu

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -3,9 +3,7 @@ const btnMobile = document.getElementById('btn-mobile')
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('nav')
-    const body = document.getElementById('body')
     nav.classList.toggle('active')
-    body.classList.toggle('active')
 }
 
 btnMobile.addEventListener('click', toggleMenu)
